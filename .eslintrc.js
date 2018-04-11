4e06c3971439330a1dedb6054a47db9751c6a4d0
Katte <417560450@qq.com>
2018-04-11 17:54:35 +0800

    2018-4-11 katte eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -66,7 +66,7 @@ module.exports = {
         'indent': 'off',
         'vue/script-indent': ['error', 4 ],
         'vue/html-indent': ['error', 4 ],
-        'no-unused-vars':['warn'],
+        'no-unused-vars':[0],
         'no-underscore-dangle':0
         // 'vuefix/vuefix': ['error', {'auto': true}]
     }
