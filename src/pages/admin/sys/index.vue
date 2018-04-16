8bea789dac5a858eed2fcd693e287046b8ca576d
Katte <417560450@qq.com>
2018-04-16 11:03:32 +0800

    2018-4-16 katte crud组件开发完成

--- src/pages/admin/sys/index.vue
+++ src/pages/admin/sys/index.vue
@@ -1,91 +1,46 @@
 <template>
-    <!-- <el-container>
-        <el-form
-            :inline="true"
-            :model="listQuery"
-            size="mini"
-            label-width="85px">
-            <el-form-item>
-                <el-button
-                    type="text"
-                    icon="el-icon-search"
-                    @click="queryList">查询</el-button>
-            </el-form-item>
-            <el-form-item>
-                <el-button
-                    type="text"
-                    icon="el-icon-plus"
-                    @click="openDialog()">添加</el-button>
-            </el-form-item>
-        </el-form>
-        <el-table
-            v-loading="tabLoading"
-            :data="tableData"
-            size="mini"
-            stripe
-            border>
-            <el-table-column
-                type="index"
-                width="36" />
-            <el-table-column
-                prop="name"
-                label="系统名称"
-                width="200px" />
-            <el-table-column
-                prop="descript"
-                label="系统描述" />
-            <el-table-column
-                label="操作"
-                align="center"
-                width="80">
-                <template slot-scope="scope">
-                    <a
-                        href="javascript:;"
-                        @click="openDialog(scope)">编辑</a>
-                    <a
-                        href="javascript:;"
-                        @click="deleteData(scope.$index, scope.row)">删除</a>
-                </template>
-            </el-table-column>
-        </el-table>
-        <update-add-dailog
-            v-bind="{isShow: showDialog, operation: operate, rowData: rowData}"
-            @toggleShow="setDialogShow" />
-
-    </el-container> -->
     <crud-page
         :columns="columns"
         :data="data"
         :query-item="queryItem"
-        @query="queryList"
-        @delete="">
+        :loading="loading"
+        @query="query"
+        @open="open"
+        @save="save"
+        @remove="remove">
 
         <template slot="queryItem">
             <el-form-item
-                label="查询字段1">
-                <el-input v-model="queryItem.search1"/>
+                label="系统名称">
+                <el-input v-model="queryItem.name"/>
             </el-form-item>
 
             <el-form-item
-                label="查询字段2">
-                <el-input v-model="queryItem.search2"/>
+                label="创建日期">
+                <el-input v-model="queryItem.createDate"/>
             </el-form-item>
         </template>
 
-        <template slot="dialogItem">
+        <template slot="editItem">
             <el-form
-                :inline="true">
-                <el-form-item>
-                    <el-input/>
+                :inline="true"
+                :model="model"
+                size="mini">
+                <el-form-item label="系统名称">
+                    <el-input
+                        v-model="model.name"
+                        style="width:220px" />
+                </el-form-item>
+                <br>
+                <el-form-item label="系统描述">
+                    <el-input
+                        v-model="model.descript"
+                        type="textarea"
+                        style="width:220px"
+                        resize="none"/>
                 </el-form-item>
             </el-form>
         </template>
-
-        <!-- <el-form-item
-            slot="queryItem"
-            label="测试插槽">
-            <el-input/>
-        </el-form-item> -->
 
     </crud-page>
 </template>
@@ -97,26 +52,10 @@ import UpdateAddDailog from './UpdateAdd'
 
 export default {
     components: {
-        UpdateAddDailog,
         CrudPage,
     },
     data () {
         return {
-            // tableData: [],
-            listQuery: {
-                filename: '',
-                // curPage: 1,
-                // pageSize: 20,
-                // importance: undefined,
-                // title: undefined,
-                // type: undefined,
-            },
-            tabLoading: false,
-            showDialog: false,
-            operate: '', // 决定弹出窗口是添加也还是修改页的变量
-            rowData: null, // 切换修改页时传过来的行数据
-
-            // 测试编辑表格插件
             columns: [
                 { name: 'name', label: '系统名称', width: 200 },
                 { name: 'descript', label: '系统描述' },
@@ -124,49 +63,70 @@ export default {
             ],
             data: [],
             queryItem: {
-                search1: '',
-                search2: '',
+                name: '',
+                createDate: '',
             },
+            loading: false,
+            model: {},
         }
     },
     mounted () {
-        this.queryList()
+        this.query()
     },
     methods: {
-        openDialog (scope) {
-            if (!scope) {
-                this.operate = 'add'
-            } else {
-                this.operate = 'update'
-                this.rowData = scope
-            }
-            this.showDialog = true
-        },
-        // 查询表格数据
-        queryList () {
-            this.tabLoading = true
+        query () {
+            this.loading = true
             api.system.query({ params: this.listQuery })
                 .then((response) => {
                     this.data = response.data
-                    this.tabLoading = false
+                    this.loading = false
                 })
                 .catch(() => {
-                    this.tabLoading = false
+                    this.loading = false
                 })
         },
-        deleteData (index, data) {
-            this.tabLoading = true
-            api.system.delete({ param: { id: data._id }, data })
-                .then(() => {
-                    this.tableData.splice(index, 1)
-                    this.tabLoading = false
-                })
-                .catch(() => {
-                    this.tabLoading = false
-                })
+        open (model) {
+            this.model = model
         },
-        setDialogShow () {
-            this.showDialog = false
+        save () {
+            const _id = this.model._id
+            if (_id) {
+                api.system.update({
+                    param: {
+                        id: _id,
+                    },
+                    data: this.model,
+                }).then((response) => {
+                    for (let i = 0; i < this.data.length; i += 1) {
+                        if (this.data[i]._id === _id) {
+                            this.data.splice(i, 1, response.data)
+                            break
+                        }
+                    }
+                })
+            } else {
+                api.system.add({
+                    data: this.model,
+                }).then((response) => {
+                    this.data.push(response.data)
+                })
+            }
+        },
+        remove (row) {
+            const { _id } = row
+            api.system.delete({
+                param: {
+                    id: _id,
+                },
+            }).then((response) => {
+                console.log(response.data.id)
+                for (let i = 0; i < this.data.length; i += 1) {
+                    if (this.data[i]._id === response.data._id) {
+                        this.data.splice(i, 1)
+                        break
+                    }
+                }
+            })
         },
     },
 }
