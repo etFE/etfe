914cf65fee365c22420a2eb633abd4b1a6b4b68c
dumuchenglin123 <243494839@qq.com>
2018-04-24 19:00:50 +0800

    2018/4/24 将之前的iconfont替换成svg cl

---
+++ src/components/admin/IconSvg.vue
@@ -0,0 +1,34 @@
+<template>
+    <svg
+        class="svg-icon"
+        aria-hidden="true">
+        <use :xlink:href="iconName"/>
+    </svg>
+</template>
+
+<script>
+export default {
+    name: 'IconSvg',
+    props: {
+        iconClass: {
+            type: String,
+            required: true,
+        },
+    },
+    computed: {
+        iconName () {
+            return `#icon-${this.iconClass}`
+        },
+    },
+}
+</script>
+
+<style scoped>
+.svg-icon {
+  width: 20px;
+  height: 20px;
+  vertical-align: middle;
+  fill: darkgray;
+  overflow: hidden;
+}
+</style>
